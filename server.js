2b360a13f9e92d512f20dcea936c41f8f35c8434
Bogdan Nikitin <bogdanonikitin@gmail.com>
2022-06-29 13:48:28 -0400

    api routes added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { User, Story } = require('./db');
 const path = require('path');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 app.use('/dist', express.static('dist'));
 
@@ -42,6 +43,46 @@ app.get('/api/users/:id/stories', async(req, res, next)=> {
   }
 });
 
+app.post('/api/users', async(req, res, next) => {
+  try {
+    res.status(201).send(await User.create(req.body));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.delete('api/users/:id', async(req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    await user.destroy();
+    res.sendStatus(204);
+  }
+  catch(ex) {
+    next(ex);
+  }
+});
+
+
+app.post('/api/users/:id/stories', async(req, res, next) => {
+  try{
+    res.status(201).send(await Story.create( { userId: req.params.userId, storyId: req.body.storyId}))
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.delete('/api/stories/:id', async(req, res, next) => {
+  try{
+    const reservation = await Story.findByPk(req.params.id);
+    await reservation.destroy();
+    res.sendStatus(204);
+  }
+  catch(ex) {
+    next(ex);
+  }
+})
 
 const port = process.env.PORT || 3000;
 
